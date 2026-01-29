ccfa6bc508f660713611bbd05e1e60ff43f2adec
SamiranDas2004 <samiran4209@gmail.com>
2026-01-29 17:33:09 +0530

    ppp

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+    <Footer />
+    <Analytics />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+import { Analytics } from '@vercel/analytics/vue'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow-x: hidden;
+}
+
+#app {
+  width: 100%;
+  overflow-x: hidden;
+}
+</style>
